1d603e1872f244fb5ec7140c2f4b4befbb6f53f0
Victor Pantoja <victor.pantoja@gmail.com>
2013-10-15 15:11:54 -0300

    Generating sprites via URLs

--- pitomba/handlers/page.py
+++ pitomba/handlers/page.py
@@ -22,14 +22,23 @@ class PageHandler(RequestHandler):
     def post(self, **kwargs):
         msg = ''
         success = True
+        urls_paths = []
+        paths = []
+        original_names = []
 
         if not self.request.files.get('file', None):
-            msg = "No file given."
-            success = False
+            if self.request.arguments.get('urls'):
+                urls_paths = self.request.arguments['urls'][0].split()
+
+                for url in urls_paths:
+                    original_names.append(url.split('/')[-1].split(".")[0])
+            else:
+                msg = "No file given."
+                success = False
         else:
-            files = []
             for r_file in self.request.files.get('file'):
                 original_fname = r_file['filename']
+                original_names.append(original_fname.split(".")[0])
                 extension = os.path.splitext(original_fname)[1]
 
                 if extension != '.png':
@@ -42,23 +51,27 @@ class PageHandler(RequestHandler):
                 with open(final_filepath, 'w') as output_file:
                     output_file.write(r_file['body'])
 
-                files.append(final_filepath)
+                paths.append(final_filepath)
 
         fname = ''.join(random.choice(string.ascii_lowercase + string.digits) for x in range(6))
         fname += '.png'
-        sprite = Sprite(files,
+
+        sprite = Sprite(paths=paths,
+                        urls_paths=urls_paths,
                         sprite_path=settings.STATIC_DEMO_PATH,
                         sprite_name=fname,
                         sprite_url=settings.SERVER_NAME + '/static/demo/',
                         css_path=settings.STATIC_DEMO_PATH,
                         class_name="demo")
 
-        css = sprite.do_write_css()
-        image = sprite.do_write_image()
+        sprite.do_write_css()
+        sprite.do_write_image()
 
         self.render("try.html",
                     SERVER_NAME=settings.SERVER_NAME,
                     message=msg,
                     success=success,
-                    css=settings.SERVER_NAME + '/static/demo/sprite.css',
-                    image=settings.SERVER_NAME + '/static/demo/' + fname)
+                    css=sprite.get_css(),
+                    css_url=settings.SERVER_NAME + '/static/demo/sprite.css',
+                    img_url=settings.SERVER_NAME + '/static/demo/' + fname,
+                    entidades=original_names)
